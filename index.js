d9cb859724b260d70eb6ab723a5c64b69192a9d9
mharb <mohamed.harb@cegedim.com>
2024-01-27 17:52:58 +0200

    allowed setting the port to listen to thorugh an environment variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.get('/generate-pdf', async (req, res) => {
     try {
